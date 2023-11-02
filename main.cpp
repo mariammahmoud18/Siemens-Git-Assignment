11fae528a3750f40b6830f6bd18809a419078926
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-11-02 20:40:23 +0200

    Adding the getMin feature to the main.cpp

--- main.cpp
+++ main.cpp
@@ -16,11 +16,19 @@ int getSum(vector<int> numbers)
 	    return vectorSum;
 }
 
+int getMin(vector<int> numbers)
+{
+	    int minElement = *min_element(numbers.begin(), numbers.end());
+
+	    return minElement;
+}
+
 int main()
 {
 	    vector<int> testVector = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 	    cout<< getSum(testVector)<<endl;
-		   
+	    cout<<getMin(testVector)<<endl;	   
+	    
 	    return 0;
 }
