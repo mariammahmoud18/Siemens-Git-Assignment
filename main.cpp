72f48583484dac0e59bb77530a29e3679d040176
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-11-02 20:36:40 +0200

    Adding the getAverage feature to the main.cpp

--- main.cpp
+++ main.cpp
@@ -16,11 +16,22 @@ int getSum(vector<int> numbers)
 	    return vectorSum;
 }
 
+float getAverage(vector<int> numbers)
+{
+	    int vectorSize = numbers.size();
+	    int vectorSum = getSum(numbers);
+            float vectorAvg = (float) vectorSum / vectorSize;
+
+            return vectorAvg;
+
+}
+
 int main()
 {
 	    vector<int> testVector = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 	    cout<< getSum(testVector)<<endl;
-		   
+            cout<< getAverage(testVector)<<endl;		   
+	    
 	    return 0;
 }
